3937e63b59116e86e220788d35b380830a00a824
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-05-13 15:33:43 +0200

    struttura

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,36 @@
 <template>
-  <div>
+  <div id="app">
+
+    <HeaderComponent />
+    <MainComponent />
+    <MidleComponent />
+    <FooterComponent />
     
   </div>
 </template>
 
 <script>
 
+import HeaderComponent from "./components/HeaderComponent.vue"
+import MainComponent from "./components/MainComponent.vue"
+import MidleComponent from "./components/MidleComponent.vue"
+import FooterComponent from "./components/FooterComponent.vue"
 
 export default {
-  
+  name: 'App',
+
   components: {
-   
+
+      HeaderComponent, 
+      MainComponent, 
+      MidleComponent, 
+      FooterComponent 
   }
 }
 </script>
 
 <style lang="scss">
-
+@import url(./assets/style/_general.scss);
+@import url(./assets/style/_utils.scss);
+@import url(./assets/style/_vars.scss);
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>Header</header>
+</template>
+
+<script>
+
+export default {
+  name: "HeaderComponent"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
