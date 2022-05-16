7d91d04046c1f341e2ec0069f227bdeda0fd351f
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-05-16 23:26:39 +0200

    finito

--- src/components/CardsComponent.vue
+++ src/components/CardsComponent.vue
@@ -1,13 +1,34 @@
 <template>
-  <div></div>
+  <div>
+    <img :src="card.thumb" :alt="card.series">
+    <p>{{card.series}}</p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "CardsComponent"
+  name: "CardsComponent",
+
+  props:{
+    card:Object
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+div{
+  flex-basis: calc(100% /6);
+  padding-top: 20px;
+  img{
+    width: 145px;
+    height: 160px;
+  }
+
+  p{
+    font-size: 15px;
+    color: white;
+  }
+}
 
 </style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,15 +1,23 @@
 <template>
   <main>
-    <div class="jumbotron"></div>
-    <CardsComponent
-      
-    />
+    <div class="jumbotron">
+      <span>CURRENT SERIES</span>
+    </div>
+
+    <div class="container cards-container">
+      <CardsComponent 
+        v-for="(cardItem,index) in comicsCards" :key="`cards-${index}`"
+        :card="cardItem"
+      />
+    </div>
+    
+    <button>LOAD MORE</button>
     
   </main>
 </template>
 
 <script>
-/* import comicsCards from "../assets/data/comicsCards" */
+import comicsCards from "../assets/data/comicsCards"
 import CardsComponent from "../components/CardsComponent.vue"
 
 export default {
@@ -17,20 +25,56 @@ export default {
 
   components: {
     CardsComponent
+  },
+
+  data(){
+    return{
+      comicsCards
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
 
+@import "../assets/style/_vars.scss";
+@import "../assets/style/_mixin.scss";
+
 main{
   background-color: #1c1c1c;
+  text-align: center;
 
   .jumbotron{
     width: 100%;
     height: 400px;
     background-image: url(../assets/img/jumbotron.jpg);
     background-size: cover;
+    position: relative;
+    span{
+      position: absolute;
+      bottom: -15px;
+      left: 230px;
+      background-color: $primary-color;
+      color: white;
+      padding: 5px 30px;
+      font-weight: bold;
+    }
+  }
+
+  .cards-container{
+    width: 70%;
+    display: flex;
+    flex-wrap: wrap;
+    padding: 20px;
+  }
+
+  button{
+    padding: 5px 30px;
+    margin-bottom: 3rem;
+    background-color: $primary-color;
+    border: none;
+    color: white;
+    font-weight: bold;
   }
 }
 </style>
